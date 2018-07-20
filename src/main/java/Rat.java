d612dba90de20d9aa790483f4e7788d33626d396
Son Nguyen <vietson6103@gmail.com>
2018-07-20 10:27:00 +0700

    add class Rat

---
+++ src/main/java/Rat.java
@@ -0,0 +1,35 @@
+public class Rat {
+    private String name;
+    private int weight;
+    private int speed;
+
+    public Rat() {
+    }
+
+    public Rat(String name, int weight, int speed) {
+        this.name = name;
+        this.weight = weight;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void run() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Rat {Name "+ getName()+". Speed "+getSpeed()+". Weight "+getWeight()+"}";
+    }
+}
