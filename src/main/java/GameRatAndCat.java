4453374dd85fad33a49d4e5f4db4d8a8357069ac
Son Nguyen <vietson6103@gmail.com>
2018-07-20 10:27:25 +0700

    add class Test

---
+++ src/main/java/GameRatAndCat.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GameRatAndCat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Cat cat = new Cat("Tom", 10);
+
+    }
+}
