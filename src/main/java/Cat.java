7277c1b6c24010b45b7c5df0c139620719e2f8af
Son Nguyen <vietson6103@gmail.com>
2018-07-20 10:27:13 +0700

    add class Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,26 @@
+public class Cat {
+    private String name;
+    private int speed;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void run() {
+
+    }
+
+
+}
